4cd62247ac4a3388d91bf2619bc773a9e2596c2b
Anton Ivanov <aivanov.mcu@gmail.com>
2022-12-03 22:24:23 +0300

    feat: add hide add button logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { IRootState } from '../../store';
 import { showModal } from '../../store/modalReducer';
 import { CustomButton } from '../CustomButton';
 import { AddProjectIcon } from '../icons/AddProjectIcon';
@@ -10,17 +12,25 @@ import styles from './Header.module.css';
 export const Header = () => {
   const dispatch = useDispatch();
   const location = useLocation();
-  const isProject = location.pathname === '/projects'
+  const params = useParams();
+  console.log(params);
+  const isProject = location.pathname === '/projects';
+  const projects = useSelector((state: IRootState) => state.projectReducer.projects);
+  let current;
+  current = !isProject ? projects.find((project: any) => project.id === params.project) : undefined;
+  let isDone = false;
+  isDone = !isProject ? current?.status === 'done' : isDone;
+  console.log(isDone);
   const itemId = `new${isProject ? 'project' : 'task'}`
   return (
     <header className={styles.headerBox}>
       <Link to='/'>
         <h1 className={styles.logo}>TODO App</h1>
       </Link>
-      <CustomButton style={styles.CustomBtn} handleClick={() => dispatch(showModal(itemId))}>
+      {(!isDone || isProject) && <CustomButton style={styles.CustomBtn} handleClick={() => dispatch(showModal(itemId))}>
         {isProject ? <AddProjectIcon /> : <AddTaskIcon />}
         <div>{'Создать нов'}{isProject ? 'ый проект' : 'ую задачу' }</div>
-      </CustomButton>
+      </CustomButton>}
     </header>
   )
 }
